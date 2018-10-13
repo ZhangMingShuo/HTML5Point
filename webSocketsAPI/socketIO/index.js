2eb618e924218e4bf8140551e1ff007da99c281f
yy709593266 <709593266@qq.com>
2018-10-13 12:59:40 +0800

    'socket.io实例'

--- webSocketsAPI/socketIO/index.js
+++ webSocketsAPI/socketIO/index.js
@@ -1,2 +1,22 @@
 let app = require('express')()
 let http = require('http').Server(app)
+let io = require('socket.io')(http)
+
+app.get('/', (req, res)=>{
+  //expressAPI方法读取文件
+  //fs.readFile是使用node的核心文件系统模块读取文件
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket)=>{
+  console.log('a user connected')
+  //客户端通过chat message事件传值过来
+  socket.on('chat message', (msg)=>{
+    //将值再发送给客户端
+    io.emit('chat message', msg)
+  })
+})
+
+http.listen(3000, ()=>{
+  console.log('listening on http://localhost:3000')
+})
